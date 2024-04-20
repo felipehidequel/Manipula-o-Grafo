89ac6d4301d576df93db5e38dd61aad359c938d8
Felipe Hidequel <felipehidequel@gmail.com>
2024-04-20 18:21:02 -0300

    added question 5,6,7

--- zero.cpp
+++ zero.cpp
@@ -76,7 +76,7 @@ void maiorGrau(std::vector<std::vector<int>> matriz) {
 }
 
 void salvarVerticeGrau(std::vector<std::vector<int>> matriz) {
-  std::ofstream arquivo("dados_grafos_graus.txt", std::ios::app);
+  std::ofstream arquivo("dados_grafos_graus.txt");
   for (int i = 0; i < N; i++) {
     arquivo << "Vertice: " << i << " Grau: " << grauVertice(matriz, i)
             << std::endl;
@@ -118,15 +118,88 @@ bool verticeSumidouro(const std::vector<std::vector<int>> &matriz) {
   return false;
 }
 
+void verticesFonte(const std::vector<std::vector<int>> &matriz) {
+  std::vector<int> verticesFonte;
+  for (int i = 0; i < N; i++) {
+    bool isFonte = true;
+    for (int j = 0; j < N; j++) {
+      if (matriz[j][i] != 0) {
+        isFonte = false;
+        break;
+      }
+    }
+    if (isFonte) {
+      verticesFonte.push_back(i);
+    }
+  }
+  if (verticesFonte.empty()) {
+    std::cout << "Não existem vértices fonte." << std::endl;
+  } else {
+    std::cout << "Vértices fonte: ";
+    for (int vertex : verticesFonte) {
+      std::cout << vertex << " ";
+    }
+    std::cout << std::endl;
+  }
+}
+
+void grauEmissaoRecepcao(const std::vector<std::vector<int>> &matriz) {
+  std::ofstream arquivoEmissao("dados_grafos_emissao.txt");
+  std::ofstream arquivoRecepcao("dados_grafos_recepcao.txt");
+
+  for (int i = 0; i < N; i++) {
+    int grauEmissao = 0;
+    int grauRecepcao = 0;
+
+    for (int j = 0; j < N; j++) {
+      grauEmissao += matriz[i][j];
+      grauRecepcao += matriz[j][i];
+    }
+
+    arquivoEmissao << "Vertice: " << i << " Grau de Emissao: " << grauEmissao
+                   << std::endl;
+    arquivoRecepcao << "Vertice: " << i << " Grau de Recepcao: " << grauRecepcao
+                    << std::endl;
+  }
+
+  arquivoEmissao.close();
+  arquivoRecepcao.close();
+}
+
+void grafoComplementar(const std::vector<std::vector<int>> &matriz) {
+  std::ofstream arquivo("grafo_complementar.txt");
+  for (int i = 0; i < N; i++) {
+    for (int j = 0; j < N; j++) {
+      if (i != j) {
+        if (matriz[i][j] == 0) {
+          arquivo << "1 ";
+        } else {
+          arquivo << "0 ";
+        }
+      }
+    }
+    arquivo << std::endl;
+  }
+  arquivo.close();
+}
+
 int main() {
   std::vector<std::vector<int>> matriz = lerGrafo("dadosmatriz.txt");
   maiorGrau(matriz);         // QUESTÃO 1
   salvarVerticeGrau(matriz); // QUESTÃO 2
   verticesIsolados(matriz);  // QUESTÃO 3
+
   // QUESTÃO 4
   if (verticeSumidouro(matriz))
     std::cout << "Existe um vertice sumidouro" << std::endl;
   else
     std::cout << "Não existe um vertice sumidouro" << std::endl;
+
+  verticesFonte(matriz); // QUESTÃO 5
+
+  grauEmissaoRecepcao(matriz); // QUESTÃO 6
+
+  grafoComplementar(matriz); // QUESTÃO 7
+
   return 0;
 }
